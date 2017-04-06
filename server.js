bfc756597c96ba362082ec95c8372c4012d7f8ae
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-04-06 16:45:47 +0200

    remove warmup which is not necessary for dual core vm

--- server.js
+++ server.js
@@ -5,27 +5,7 @@ var jsreport = require('jsreport')({ tempDirectory: path.join(__dirname, 'temp')
 
 
 jsreport.init().then(function () {
-  console.log('running')
-
-  // doing a first render in electron-pdf recipe to "warm-up" the process
-  // since starting our electron process on single core windows takes 100% CPU,
-  // this render is just to prevent other users experimenting a blocking render
-  console.log('doing a first render on electron-pdf recipe..')
-
-  jsreport.render({
-	template: {
-	  content: "hi",
-	  engine: "handlebars",
-	  recipe: "electron-pdf",
-	  workspaceShortid: 'HJVhE0QP',
-	  workspaceVersion: 804
-	},
-	headers: {}
-  }).then(function() {
-	console.log('first render in electron-pdf recipe ended successfully')
-  }).catch(function(err) {
-	console.log('first render in electron-pdf recipe has an error:', err.message)
-  })
+  console.log('running')  
 }).catch(function (e) {
   console.error(e)
 })
