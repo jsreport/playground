e2caecc75ad34bfc7792b1edd74416cdbb891a95
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-04-09 16:58:50 +0200

    improve examples

--- studio/Startup.js
+++ studio/Startup.js
@@ -16,49 +16,78 @@ export default class Startup extends Component {
 
   renderExamples () {
     return <table className='table'>
-      <tbody>
+      <tbody>     
         <tr>
-          <td className='selection'>
-            <a href='/studio/workspace/HJVhE0QP/804' target='_blank'>Hello world</a></td>
+          <td className='selection'><a href='/studio/workspace/SyUrRILTg/6' target='_blank'>Invoice pdf</a></td>
         </tr>
         <tr>
-          <td className='selection'><a href='/studio/workspace/lkHFBn0xB/1818' target='_blank'>Example Pdf Report</a>
+          <td className='selection'><a href='/studio/workspace/SkDFywLpe/3' target='_blank'>Orders with data fetch script</a></td>
+        </tr>
+        <tr>                  
+          <td className='selection'><a href='/studio/workspace/ry0peD8pl/9' target='_blank'>Population Excel</a></td>
+        </tr>                     
+        <tr>                  
+          <td className='selection'><a href='/studio/workspace/HkqlE-Ww/205' target='_blank'>Live dashboard</a>
           </td>
+        </tr>     
+        <tr>
+          <td className='selection'><a href='/studio/workspace/Y3BG0fnPa/1' target='_blank'>Html table to Excel</a></td>
         </tr>
         <tr>
-          <td className='selection'><a
-            href='/studio/workspace/HkqlE-Ww/205' target='_blank'>Live dashboard</a>
-          </td>
+          <td className='selection'><a href='/studio/workspace/gkxJuycgR5/1' target='_blank'>Pdf rendering with Apache FOP</a></td>
         </tr>
         <tr>
-          <td className='selection'><a href='/studio/workspace/l1DbOPsN5/2218' target='_blank'>Pdf Invoice</a></td>
-        </tr>
-        <tr>
-          <td className='selection'><a href='/studio/workspace/r1G-1ULP/3' target='_blank'>Server script loading report data</a></td>
-        </tr>
-        <tr>
-          <td className='selection'><a
-            href='/studio/workspace/r1vaurbw/3' target='_blank'>Add row to Excel</a>
-          </td>
-        </tr>
-        <tr>
-          <td className='selection'><a href='/studio/workspace/HyQH-eKv/115' target='_blank'>Excel pivot table</a></td>
-        </tr>
-        <tr>
-          <td className='selection'><a href='/studio/workspace/gkxJuycgR5/1' target='_blank'>Apache FOP</a></td>
-        </tr>
-        <tr>
-          <td className='selection'><a href='/studio/workspace/Y3BG0fnPa/1' target='_blank'>Html to xlsx</a></td>
-        </tr>
+          <td className='selection'><a href='/studio/workspace/ZJZMyHgm2e/2' target='_blank'>Chrome based pdf rendering using electron</a></td>
+        </tr>   
+         <tr>
+          <td className='selection'><a href='/studio/workspace/bkBXJqNOae/14' target='_blank'>wkhtmltopdf with page numbers</a></td>
+        </tr>       
         <tr>
           <td className='selection'><a href='/studio/workspace/Y3QQDfP9a/34' target='_blank'>csv</a></td>
+        </tr>  
+        <tr>
+          <td className='selection'><a href='/studio/workspace/HkM7PSFSl/15' target='_blank'>Template layouts</a></td>
         </tr>
         <tr>
-          <td className='selection'><a href='/studio/workspace/bkBXJqNOae/14' target='_blank'>wkhtmltopdf page numbers</a></td>
+          <td className='selection'><a href='/studio/workspace/HyGQQ-KHl/8' target='_blank'>Custom font in pdf</a></td>
+        </tr>         
+        <tr>
+          <td className='selection'><a href='/studio/workspace/gyHJRWnpn/79' target='_blank'>Pdf page breaks</a></td>
+        </tr>                                                                        
+        <tr>
+          <td className='selection'><a href='/studio/workspace/r1vaurbw/3' target='_blank'>Add row to Excel</a>
+          </td>
         </tr>
         <tr>
-          <td className='selection'><a href='/studio/workspace/ZJZMyHgm2e/2' target='_blank'>electron pdf</a></td>
+          <td className='selection'><a href='/studio/workspace/ryaUvq21e/3' target='_blank'>Excel debug</a></td>
+        </tr>         
+        <tr>
+          <td className='selection'><a href='/studio/workspace/Hkr4xanxg/7' target='_blank'>Update excel cell</a></td>
+        </tr>  
+        <tr>
+          <td className='selection'><a href='/studio/workspace/rkWcRiHog/53' target='_blank'>Dynamic formula in excel</a></td>
+        </tr> 
+        <tr>
+          <td className='selection'><a href='/studio/workspace/BkcNMahxg/6' target='_blank'>Auto recalculate excel formulas</a></td>
+        </tr>  
+        <tr>
+          <td className='selection'><a href='/studio/workspace/Hy_V2BSh/4' target='_blank'>Excel table</a></td>
+        </tr> 
+        <tr>
+          <td className='selection'><a href='/studio/workspace/SyL6aErP/2' target='_blank'>Add sheet to excel</a></td>
+        </tr>          
+        <tr>
+          <td className='selection'><a href='/studio/workspace/rkX89bHD/2' target='_blank'>Merged excel cells</a></td>
         </tr>
+        <tr>
+          <td className='selection'><a href='/studio/workspace/H1BHqBZw/9' target='_blank'>Conditional excel formatting</a></td>
+        </tr> 
+        <tr>
+          <td className='selection'><a href='/studio/workspace/BJa5OBWD/2' target='_blank'>Rename excel sheet</a></td>
+        </tr> 
+        <tr>
+          <td className='selection'><a href='/studio/workspace/HyQH-eKv/115' target='_blank'>Excel pivot table</a></td>
+        </tr>        
       </tbody>
     </table>
   }
@@ -91,11 +120,14 @@ export default class Startup extends Component {
       <h2>quick start</h2>
       <button className='button confirmation' onClick={() => Studio.openNewModal('templates')}>
         <i className='fa fa-plus' /> Create template
-      </button>
-      <h2>last created workspaces</h2>
-      {this.renderLastCreated()}
+      </button>    
+      <button className='button confirmation' onClick={() => this.refs.lastCreatedWorkspaces.scrollIntoView()}>
+        <i className='fa fa-user' /> Last created workspaces
+      </button>    
       <h2>playground samples</h2>
       {this.renderExamples()}
+      <h2 ref="lastCreatedWorkspaces">last created workspaces</h2>
+      {this.renderLastCreated()}
     </div>
   }
 }
