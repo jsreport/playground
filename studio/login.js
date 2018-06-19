b87b0c9ba65879a2e744a8a03c8d5081535e6e09
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-06-19 10:03:04 +0200

    add github login

--- studio/login.js
+++ studio/login.js
@@ -4,8 +4,8 @@ export default (props = {}) => {
   })
 
   return <div>
-    <a target='_top' onClick={() => onClick('/login/facebook')} style={{ color: '#3b5998', marginRight: '0.5rem', cursor: 'pointer' }}><i className='fa fa-facebook-square fa-3x' /></a>
-    <a target='_top' onClick={() => onClick('/login/twitter')} style={{ color: '#2fc2ef', marginRight: '0.5rem', cursor: 'pointer' }}><i className='fa fa-twitter-square fa-3x' /></a>
-    <a target='_top' href='/auth/github' style={{ color: '#337ab7' }}><i className='fa fa-github fa-3x' /></a>
+    <a target='_top' title='facebook' onClick={() => onClick('/login/facebook')} style={{ color: '#3b5998', marginRight: '0.5rem', cursor: 'pointer' }}><i className='fa fa-facebook-square fa-3x' /></a>
+    <a target='_top' title='twitter' onClick={() => onClick('/login/twitter')} style={{ color: '#2fc2ef', marginRight: '0.5rem', cursor: 'pointer' }}><i className='fa fa-twitter-square fa-3x' /></a>
+    <a target='_top' title='github' onClick={() => onClick('/login/github')} style={{ color: '#337ab7', marginRight: '0.5rem', cursor: 'pointer' }}><i className='fa fa-github fa-3x' /></a>
   </div>
 }
