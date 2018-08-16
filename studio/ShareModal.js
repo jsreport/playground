dd5031e3a010ab7327c7509d1f7e4e67cd86b029
pofider <jan.blaha@hotmail.com>
2018-08-16 12:42:01 +0200

    better explanation in the share dialog

--- studio/ShareModal.js
+++ studio/ShareModal.js
@@ -4,13 +4,20 @@ export default class ShareModal extends Component {
   render () {
     return (
       <div style={{padding: '1.5rem'}}>
+        <h3>simple link</h3>
         <p>
-          <b><i className='fa fa-code' />&nbsp;Use the following code to embed your workspace in another page:</b>
+          You can just copy paste browser url to share the playground workspace:
+          <br />
+          <a href='{window.location.href}'>{window.location.href}</a>
         </p>
-        <br />
-        <code style={{ backgroundColor: 'wheat' }}>
+        <h3>embed into website</h3>
+        <p>
+          If you want embed playgorund into another page, you can use this html code:
+          <br />
+          <code style={{ backgroundColor: 'wheat' }}>
           &lt;iframe src=&quot;{window.location.href}?embed=1&quot; width=&quot;100%&quot; height=&quot;400&quot; frameborder=&quot;0&quot;&gt;&lt;/iframe&gt;
-        </code>
+          </code>
+        </p>
       </div>
     )
   }
